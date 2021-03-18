02fa505e37d5c042f620b47ddfda7811d8eebea5
stacksonstack <staceycarrillo44@gmail.com>
2021-03-17 22:47:16 -0400

    second challenge done

--- March17.js
+++ March17.js
@@ -15,3 +15,37 @@ function solveMeFirst(a, b) {
 
 // console.log(solveMeFirst(7, 3));
 
+
+
+
+// This is a staircase of size n=4 :
+
+//    #
+//   ##
+//  ###
+// ####
+// Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+// Write a program that prints a staircase of size .
+
+// Function Description
+
+// Complete the staircase function in the editor below.
+
+// staircase has the following parameter(s):
+
+// int n: an integer
+
+function staircase(n) {
+    let filledArray = new Array(n).fill(' '); //makes an array with n empty strings
+    console.log(filledArray)
+    while (filledArray.indexOf(' ') !== -1) {
+        filledArray.shift();
+        filledArray.push('#');
+        console.log(filledArray.join("")); // the while loop returns what we want 
+        n--;
+    }
+    return
+}
+
+console.log(staircase(10))
