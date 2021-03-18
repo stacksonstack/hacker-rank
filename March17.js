7b5b2e51e918fa53e3554e64e59ed709264d6463
stacksonstack <staceycarrillo44@gmail.com>
2021-03-17 22:04:34 -0400

    first challenge done

---
+++ March17.js
@@ -0,0 +1,17 @@
+// Complete the solveMeFirst function in the editor below.
+
+// solveMeFirst has the following parameters:
+
+// int a: the first value
+// int b: the second value
+// Returns
+// - int: the sum of a and b
+
+
+
+function solveMeFirst(a, b) {
+  return a + b  
+}
+
+// console.log(solveMeFirst(7, 3));
+
