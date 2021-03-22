bb30bfab90199016b864844320e85ba86181988a
stacksonstack <staceycarrillo44@gmail.com>
2021-03-21 20:43:54 -0400

    second challenge done

--- March21.js
+++ March21.js
@@ -28,4 +28,34 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
   return `${appleCount} \n${orangeCount}`
 }
 
-console.log(countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4]));
+// console.log(countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4])); // 1 , 2
+
+
+
+
+// HackerLand University has the following grading policy:
+
+// Every student receives a grade in the inclusive range from 0 to 100.
+// Any grade less than 40 is a failing grade.
+// Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+// If the difference between the grade and the next multiple of 5 is less than 3, round  up to the next multiple of 5.
+// If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+// Examples
+
+// grade4 = 84 round to 85 (85 - 84 is less than 3)
+// grade = 29 do not round (result is less than 40)
+// grade = 57 do not round (60 - 57 is 3 or higher)
+// Given the initial value of  for each of Sam's  students, write code to automate the rounding process.
+
+function gradingStudents(grades) {
+    return grades.map((grade) => {
+        if(grade >= 38){
+            if(grade%5 == 3 ) return grade+=2
+            if(grade%5 == 4 ) return grade+=1
+        }
+        return grade
+    })
+}
+
+console.log(gradingStudents([73, 67, 38, 33])) //75, 67, 40 , 33
