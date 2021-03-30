2be9e8c4633af5dc673c4cbefc93714e2841dfb2
stacksonstack <staceycarrillo44@gmail.com>
2021-03-29 20:34:19 -0400

    second challenge do ne

--- March29.js
+++ March29.js
@@ -24,3 +24,28 @@ function birthday(s, d, m) {
 }
 
 // console.log(birthday([2,2,1,3,2],4,2))
+
+
+// Function Description
+
+// Complete the pickingNumbers function in the editor below.
+
+// pickingNumbers has the following parameter(s):
+
+// int a[n]: an array of integers
+// Returns
+
+// int: the length of the longest subarray that meets the criterion
+
+function pickingNumbers(a) {
+    let maxcount = 0;
+    [...new Set(a)].forEach(x => {
+        maxcount = Math.max(maxcount,
+            a.reduce((c, v) => c += (v === x || v === x + 1), 0));
+    })
+    return maxcount;
+}
+
+// console.log(pickingNumbers([1,1,2,2,4,4,5,5,5]))
+// console.log(pickingNumbers([4,6,5,3,3,1]))
+// console.log(pickingNumbers([1, 2, 2, 3, 1 ,2]))
