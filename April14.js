7df9f7bae1f98b89faa27dc0883434c96d92afb3
stacksonstack <staceycarrillo44@gmail.com>
2021-04-14 22:25:59 -0400

    third challenge completed

--- April14.js
+++ April14.js
@@ -30,10 +30,48 @@ function plusMinus(arr) {
     // 0.500000
     // 0.333333
     // 0.166667
-    return(`${d['1']/arr.length}\n`, `${d['-1']/arr.length}\n`, `${d['0']/arr.length}`  )
+    return `${(d['1']/arr.length).toFixed(6)}\n${(d['-1']/arr.length).toFixed(6)}\n${(d['0']/arr.length).toFixed(6)}`  
     
 }
 
 // console.log(plusMinus([-4,3,-9,0,4,1])) // 0.5 \n 0.33333 \n , 0.166667
 
 
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+function miniMaxSum(arr) {
+    let sorted = arr.sort()
+    return [sorted.slice(0,arr.length -1).reduce((acc, curr)=> acc + curr), sorted.slice(1).reduce((acc, curr)=> acc + curr)]
+}
+
+// console.log(miniMaxSum([1,2,3,4,5]))
+
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+// For example, the square matrix arr is shown below:
+
+function diagonalDifference(arr) {
+    let myArray = 0
+    let myArray2 = 0
+    for(let i = 0; i < arr.length; i++){
+        var index = arr.length - 1
+        myArray += arr[i][i]
+        myArray2 += arr[i][index - i]
+    }
+    return Math.abs(myArray - myArray2)
+}
+
+// console.log(diagonalDifference([ [ 11, 2, 4 ], [ 4, 5, 6 ], [ 10, 8, -12 ] ]))
+
+function _diagonalDifference(arr) {
+    var indexLength = arr.length - 1
+    var diagonal = arr.reduce((acc, curr, index) => {
+        console.log(curr)
+        acc[0] += curr[index]
+        acc[1] += curr[indexLength-index]
+        return acc
+    }, [0,0])
+    return Math.abs(diagonal.reduce((acc, curr) => acc - curr))
+}
+
+console.log(_diagonalDifference([ [ 11, 2, 4 ], [ 4, 5, 6 ], [ 10, 8, -12 ] ]))
