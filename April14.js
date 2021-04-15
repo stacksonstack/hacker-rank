1a9190415bba1742b27f6a77cb7024ec8e2914a8
stacksonstack <staceycarrillo44@gmail.com>
2021-04-14 21:26:31 -0400

    second challenge done

--- April14.js
+++ April14.js
@@ -16,3 +16,24 @@ function fizzBuzz(n) {
 }
 
 // fizzBuzz(15) 
+
+
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line.
+function plusMinus(arr) {
+    let d = arr.reduce((acc, curr)=> {
+        if (curr > 0) acc['1']+= 1 
+        else if (curr < 0) acc['-1'] += 1
+        else acc['0'] += 1
+        return acc
+    },{'1': 0, '-1': 0, '0': 0})
+    // arr= [-4, 3, -9, 0, 4, 1]
+    // 0.500000
+    // 0.333333
+    // 0.166667
+    return(`${d['1']/arr.length}\n`, `${d['-1']/arr.length}\n`, `${d['0']/arr.length}`  )
+    
+}
+
+// console.log(plusMinus([-4,3,-9,0,4,1])) // 0.5 \n 0.33333 \n , 0.166667
+
+
