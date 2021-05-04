584d4f52e54d2a5ad290f61bf89f19604bc876f4
stacksonstack <staceycarrillo44@gmail.com>
2021-05-03 22:16:21 -0400

    second challenge done

--- May3.js
+++ May3.js
@@ -21,3 +21,26 @@ function _saveThePrisoner(n, m, s) {
     
 // console.log(_saveThePrisoner(4,6,2))
 
+
+
+// Lily likes to play games with integers. She has created a new game where she determines the difference between a number and its reverse. For instance, given the number 12, its reverse is 21. Their difference is 9. The number 120 reversed is 21, and their difference is 99.
+
+// She decides to apply her game to decision making. She will look at a numbered range of days and will only go to a movie on a beautiful day.
+
+// Given a range of numbered days, [i..j]  and a number k, determine the number of days in the range that are beautiful. Beautiful numbers are defined as numbers where |i-reverse(i)| is evenly divisible by . If a day's value is a beautiful number, it is a beautiful day. Return the number of beautiful days in the range.
+
+
+function beautifulDays(i, j, k) {
+    var myArray = []
+    var beautifulDays = 0
+    while(i <= j){
+        myArray.push(i)
+        i++
+    }
+    myArray.forEach((num)=> {
+        if((num - num.toString().split("").reverse().join("")) % k === 0 ) beautifulDays+=1
+    })
+return beautifulDays
+}
+
+console.log(beautifulDays(20,23,6))
